7ffd7275790a3dbf0abeb9b92164f22b01ef6ed5
Audrey Klammer <audrey.ck@gmail.com>
2023-02-14 14:14:04 -0800

    layout styled components and footer from original site all work, no errors

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,146 @@
 import React from 'react'
-
 import Container from './container'
-import * as styles from './footer.module.css'
 
-const Footer = () => (
-  <Container as="footer">
-    <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
-    </div>
-  </Container>
-)
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
+import { SocialIcon } from 'react-social-icons'
+
+const FooterGrid = styled.nav`
+  max-width: 100vw;
+  margin: 0 auto;
+  display: grid;
+  padding: 3vw;
+  grid-template-columns: 1fr auto auto;
+  grid-auto-columns: 1fr;
+  grid-column-gap: 8vw;
+  grid-row-gap: 16px;
+  @media (max-width: 991px) {
+    grid-template-columns: 1fr;
+    padding: 4vw;
+    grid-row-gap: 40px;
+    grid-template-rows: auto auto auto;
+  }
+`
+
+const FooterNav = styled.div`
+  display: flex;
+  align-self: start;
+  flex-direction: row;
+  @media (max-width: 991px) {
+    margin-right: 20px;
+  }
+`
+
+const StyledLink = styled(Link)`
+  color: #fff;
+  font-size: 17px;
+  line-height: 24px;
+  margin-right: 3vw;
+  @media (max-width: 991px) {
+    margin-right: 20px;
+  }
+`
+
+const FooterLinks = styled.div`
+  display: flex;
+  padding-right: 4vw;
+  flex-direction: column;
+  align-self: start;
+  @media (max-width: 991px) {
+    flex-direction: row;
+  }
+`
+
+const StyledFooterLinks = styled.div`
+  color: #fff;
+  font: normal 600 13px/10px Inter, sans-serif;
+  margin: 6px 0;
+  padding: 6px 0;
+  @media (max-width: 991px) {
+    flex-direction: row;
+    margin-right: 20px;
+  }
+`
+
+const StyledHeader = styled.h3`
+  margin: 0 0 12px 0;
+  color: white;
+`
+
+const StyledText = styled.div`
+  padding: 0;
+  margin-bottom: 12px;
+`
+
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          social {
+            twitter
+            github
+            linkedin
+          }
+        }
+      }
+    }
+  `)
+
+  const { twitter, github, linkedin } = data.site.siteMetadata.social
+
+  return (
+    // <Container as="footer">
+    <FooterGrid>
+      <FooterNav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/portfolio">Portfolio</StyledLink>
+        {/*<StyledLink to="/about">About</StyledLink>*/}
+        <StyledLink to="/blog">Blog</StyledLink>
+        {/*<StyledLink to="/contact">Contact</StyledLink>*/}
+      </FooterNav>
+
+      <div>
+        <StyledHeader> Let's work together</StyledHeader>
+        <StyledText>Have a project in mind? Don’t hesitate to contact me.</StyledText>
+      </div>
+
+      <FooterLinks>
+        <StyledFooterLinks>
+          <SocialIcon
+            bgColor="white"
+            style={{ height: 20, width: 20, marginRight: '6px' }}
+            url={twitter}
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          Twitter
+        </StyledFooterLinks>
+        <StyledFooterLinks>
+          <SocialIcon
+            bgColor="white"
+            style={{ height: 20, width: 20, marginRight: '6px' }}
+            target="_blank"
+            url={github}
+            rel="noopener noreferrer"
+          />
+          GitHub
+        </StyledFooterLinks>
+        <StyledFooterLinks>
+          <SocialIcon
+            bgColor="white"
+            style={{ height: 20, width: 20, marginRight: '6px' }}
+            url={linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          LinkedIn
+        </StyledFooterLinks>
+      </FooterLinks>
+    </FooterGrid>
+    // </Container>
+  )
+}
 
 export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import './variables.css'
 import './global.css'
 import Seo from './seo'
 import Navigation from './navigation'
 import Footer from './footer'
+
+const Site = styled.div`
+  background: white;
+  min-height: 100vh;
+  display: grid;
+  grid:
+    auto-flow minmax(4em, auto) 1fr minmax(1em, auto) / 2em 1fr minmax(auto, 60em)
+    1fr 2em;
+`
+
+const SiteHeader = styled.header`
+  color: black;
+  grid-column: span 5;
+  grid-row: 1;
+`
+
+const SiteMain = styled.main`
+  background: white;
+  grid-column: span 5;
+`
+
+const SiteFooter = styled.footer`
+  grid-column: span 5;
+  background-color: #433056;
+  color: #fff;
+  padding: 0;
+`
+
+const FooterBottom = styled.div`
+  background-color: rgba(0, 0, 0, 0.08);
+  padding: 1.6vw 3vw;
+  @media (max-width: 991px) {
+    padding: 5vw 3vw;
+  }
+`
+
 class Template extends React.Component {
   render() {
     const { children } = this.props
 
     return (
       <>
-        <Seo />
-        <Navigation />
-        <main>{children}</main>
-        <Footer />
+        <Site>
+          <Seo />
+          <SiteHeader role="banner">
+            <Navigation />
+          </SiteHeader>
+          <SiteMain role="main">
+            {children}
+          </SiteMain>
+          <SiteFooter role="contentinfo">
+            <Footer />
+            <FooterBottom>
+              Handcrafted by © Audrey Klammer - {new Date().getFullYear()}
+            </FooterBottom>
+          </SiteFooter>
+        </Site>
       </>
     )
   }
