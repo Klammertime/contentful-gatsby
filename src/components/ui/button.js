84785fcf7cb7edbf9efd6693349b15fc6e4d3ac8
Audrey Klammer <audrey.ck@gmail.com>
2023-04-21 03:16:52 -0700

    adding font

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -2,28 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-  margin-top: 16px;
-  color: white;
   display: flex;
   align-items: center;
   height: 48px;
+  margin-top: 16px;
   padding: 13px 24px;
+  color: white;
   font-weight: 700;
   font-size: 15px;
   line-height: 20px;
+  text-align: center;
   text-decoration: none;
   background-color: var(--primary);
-  border-radius: 24px;
-  text-align: center;
   border: 0 none;
+  border-radius: 24px;
   cursor: pointer;
   transition: color 400ms cubic-bezier(0.25, 0.46, 0.45, 0.94),
     background-color 400ms cubic-bezier(0.25, 0.46, 0.45, 0.94),
     border-color 400ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
   &:hover {
-    background-color: var(--dark-purple);
     color: var(--white);
+    background-color: var(--dark-purple);
     outline: 0;
   }
 `
